57a2c86c0f2c2bae9591779220e5ce62b458da51
RogerioJunor <rogeriopcjr@gmail.com>
2022-08-05 04:40:34 -0300

    create components layout and services axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-
+import React from "react";
+import Layout from "./components/layout";
 
 function App() {
   return (
-    <div className="App">
-      github api
-    </div>
+    <main>
+      <Layout>
+        <div>
+          <img source="" alt="Avatar of user" />
+          <h1>Name User</h1>
+          <h3>Username: </h3>
+          <span>username</span>
+          <div>
+            <h4>Followers</h4>
+            <span>quantity folloewers</span>
+          </div>
+          <div>
+            <h4>Starreds</h4>
+            <span>quantity starreds</span>
+          </div>
+          <div>
+            <h4>Followings</h4>
+            <span>quantity folloewings</span>
+          </div>
+        </div>
+        <div>Repositories</div>
+        <div>Starreds</div>
+      </Layout>
+    </main>
   );
 }
 
