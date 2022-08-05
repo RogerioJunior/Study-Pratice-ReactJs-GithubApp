325dc44397e4820fc3bbe5d1a2be8d43f74e0b4b
RogerioJunor <rogeriopcjr@gmail.com>
2022-08-05 05:14:28 -0300

    styling profile

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
 import React from "react";
 import Layout from "./components/layout";
+import Profile from "./components/profile";
 
-function App() {
+const App = () => {
   return (
     <main>
       <Layout>
-        <div>
-          <img source="" alt="Avatar of user" />
-          <h1>Name User</h1>
-          <h3>Username: </h3>
-          <span>username</span>
-          <div>
-            <h4>Followers</h4>
-            <span>quantity folloewers</span>
-          </div>
-          <div>
-            <h4>Starreds</h4>
-            <span>quantity starreds</span>
-          </div>
-          <div>
-            <h4>Followings</h4>
-            <span>quantity folloewings</span>
-          </div>
-        </div>
+        <Profile></Profile>
         <div>Repositories</div>
         <div>Starreds</div>
       </Layout>
     </main>
   );
-}
+};
 
 export default App;
---
+++ src/components/profile/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import * as S from './styled'
+
+const Profile = () => {
+    return (
+        <S.Wrapper>
+            <S.WrapperImage src="" alt="Avatar of user" />
+            <S.WrapperInfoUser>
+                <div>
+                    <h1>Name User</h1>
+                    <S.WrapperUsername>
+                        <h3>Username:</h3>
+                        <span>username</span>
+                    </S.WrapperUsername>
+                </div>
+                <S.WrapperStatusCount>
+                    <div>
+                        <h4>Followers</h4>
+                        <span>2</span>
+                    </div>
+                    <div>
+                        <h4>Starreds</h4>
+                        <span>3</span>
+                    </div>
+                    <div>
+                        <h4>Followings</h4>
+                        <span>4</span>
+                    </div>
+                </S.WrapperStatusCount>
+            </S.WrapperInfoUser>
+        </S.Wrapper>
+    );
+};
+
+export default Profile
